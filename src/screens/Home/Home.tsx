2fb38c54a707fa3ed60608425f83717ef069d50e
David Vinter <rt56223@gmail.com>
2022-01-07 17:28:55 +0200

    run back hasky

--- src/screens/Home/Home.tsx
+++ src/screens/Home/Home.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import { Text } from "react-native";
 import { styles } from "./styles";
 const HomeScreen = () => {
-  return <Text style={styles.text}>HomeScreen
-  </Text>;
+  return <Text style={styles.text}>HomeScreen</Text>;
 };
 
 export default HomeScreen;
