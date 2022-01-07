3c5be276fb15c28c7f13374eb16b25f4fcbf8f77
David Vinter <rt56223@gmail.com>
2022-01-07 17:17:05 +0200

    Add prettier eslint husky for better coding

---
+++ src/screens/Home/Home.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { Text } from "react-native";
+import { styles } from "./styles";
+const HomeScreen = () => {
+  return <Text style={styles.text}>HomeScreen</Text>;
+};
+
+export default HomeScreen;
